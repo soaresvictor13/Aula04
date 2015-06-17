dda01cec2ba0239a9c7629f10b6a315aebad7f6f
samuel <samukahuss2009@gmail.com>
2015-06-16 21:45:57 -0300

    Implementacao dos controllers

--- app/src/main/java/br/eti/victorsoares/aula04/smldao/ItensAmigoDAO.java
+++ app/src/main/java/br/eti/victorsoares/aula04/smldao/ItensAmigoDAO.java
@@ -18,9 +18,11 @@ public class ItensAmigoDAO implements modeloDAO {
 
     protected static final String SCRIPT_CREATE="CREATE TABLE ItensAmigos(" +
             "_cod_item INTEGER PRIMARY KEY AUTOINCREMENT," +
-            "FOREIGN KEY (_cod_amigo) REFERENCES Amigos(_cod_amigo),"+
+            "_cod_amigo INTEGER,"+
             "descricao_item TEXT,"+
-            "nome_item TEXT;";
+            "nome_item TEXT,"+
+            "FOREIGN KEY (_cod_amigo) REFERENCES Amigos(_cod_amigo);";
+
     private AcessoDB acessoDB;
 
     @Override
